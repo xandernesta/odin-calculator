8ab7c71952c4ad79503ff32e4f0b61ddcbad92bb
Alex N <xander.nesta@gmail.com>
2022-08-22 14:25:08 -0400

    create the 4 calculation functions

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+function add(num1, num2){
+    return  result = num1 + num2;
+}
+function subtract(num1, num2){
+    return  result = num2 - num1;
+}
+function multiply(num1, num2){
+    return  result = num1 * num2;
+}
+function divide(num1, num2){
+    return  result = num1 / num2;
+}
